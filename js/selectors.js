4964e523361e9f37f874db0725fbeb561c9ffaa6
Jared Chamberlain <jchamberlain909@gmail.com>
2018-04-23 18:51:54 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -10,3 +10,9 @@ function lastImageSelector(){
 function ninjaBabySelector(){
   return $('#baby-ninja');
 }
+
+function divSelector(){
+  return $('.pics');
+}
+
+const firstListItem = () => ($('ul#pic-list:first-child'));
